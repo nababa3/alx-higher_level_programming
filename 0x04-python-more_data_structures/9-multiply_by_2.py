66fd458723d25b050c56c9090a4df09fa2d030fb
root <nababa3@github.com>
2023-06-14 21:50:12 +0000

    mutiply

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if(a_dictionary):
-        return(dict((x, y*2) for x, y in a_dictionary.items()))
+    return ({i: a_dictionary[i] * 2 for i in a_dictionary})
