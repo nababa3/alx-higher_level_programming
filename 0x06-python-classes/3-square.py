abfcb10bf33a237143a0a90c662496d53fd7d40b
Nababa ABdulrahmon <nababa3@github.com>
2023-06-27 19:13:33 +0000

    square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+
 """ a sqaure class"""
+
+
 class Square:
     """ a class that defines private instance variables"""
+
     _Square__size = None
+
     def __init__(self, size=0):
+
         """The constructor of the class"""
+
         self._Square__size = size
         if not isinstance(size, int):
             raise (TypeError("size must be an integer"))
