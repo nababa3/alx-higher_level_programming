755fdc470f3f9d2d0e7af6a0b8244647cc7279d3
Nababa ABdulrahmon <nababa3@github.com>
2023-07-10 12:55:04 +0000

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""lookup"""
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """returns list of dir()"""
-    return([x for x in dir(obj)])
+    """Return a list of an object's available attributes."""
+    return (dir(obj)
+
