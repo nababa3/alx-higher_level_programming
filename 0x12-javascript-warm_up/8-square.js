9a8b665d100208353c4ec9005d8d11b79090a697
Nababa ABdulrahmon <nababa3@github.com>
2023-09-11 21:33:06 +0000

    script

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const sqru = process.argv[2];
-if (isNaN(sqru)) {
-    console.log('Missing size');
+const sqr = process.argv[2];
+if (isNaN(sqr)) {
+  console.log('Missing size');
 } else {
-    for (let x = 0; x < sqru; x++) {
-        console.log('X'.repeat(sqru));
-    }
+  for (let x = 0; x < sqr; x++) {
+    console.log('X'.repeat(sqr));
+  }
 }
