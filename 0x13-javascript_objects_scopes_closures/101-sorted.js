5a901f09ed82b8344edc8e38b6626912559b0d9c
Nababa ABdulrahmon <nababa3@github.com>
2023-09-13 22:08:40 +0000

    rectangle

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-exports.dict = {
-    89: 1,
-    90: 2,
-    91: 1,
-    92: 3,
-    93: 1,
-    94: 2
-};
+const direc = require('./101-data.js').dict;
+const ndix = {};
+
+for (const k in direc) {
+  if (direc[k] in ndix) {
+    ndix[direc[k]].push(k);
+  } else {
+    ndix[direc[k]] = [k];
+  }
+}
+console.log(ndix);
