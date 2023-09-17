20510cd3b0d575e542387394f943c18661a2d6da
Nababa ABdulrahmon <nababa3@github.com>
2023-09-17 03:51:24 +0000

    object

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Creates the State “California” with the City “San Francisco”
-from the database hbtn_0e_100_usa.
-"""
-import sys
+# sql alchemy 7
 from sqlalchemy import create_engine
+from sqlalchemy import MetaData
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
-from relationship_city import Base, City
-
+from relationship_state import Base, State
+from relationship_city import City
+import sys
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    session.add(City(name="San Francisco", state=State(name="California")))
+    sf = City(name='San Francisco')
+    ca = State(name='California')
+    ca.cities.append(sf)
+    session.add(ca)
     session.commit()
+    session.close()
--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-# sql alchemy 7
-from sqlalchemy import create_engine
-from sqlalchemy import MetaData
-from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-from relationship_city import City
+"""Lists all States and corresponding Cities in the database hbtn_0e_101_usa."""
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
+
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
+
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
-    session.close()
+            print("    {}: {}".format(city.id, city.name))
