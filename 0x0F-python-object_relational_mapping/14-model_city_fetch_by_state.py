041384c8da694c049e876541cfede6cf69b9f3ad
Nababa ABdulrahmon <nababa3@github.com>
2023-09-17 03:32:36 +0000

    object

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-"""Script that deletes State objects with a name containing the letter a"""
-import sys
+"""module lists all city objects from database using SQLAlchemy
+"""
+
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
-
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for row in session.query(State, City).join(City).order_by(City.id):
-        print("{:}: ({:}) {:}".format(row[0].name, row[1].id, row[1].name))
-    session.commit()
-    session.close()
+    username = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    # setup engine
+    engine_string = "mysql://{}:{}@localhost:3306/{}".format(username,
+                                                             passwd, db)
+    # default username: root, passwd: "", db: hbtn_0e_6_usa
+    engine = create_engine(engine_string)
+    Base.metadata.bind = engine
+
+    # setup session
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    state_list = session.query(State).order_by(State.id).all()
+    city_list = session.query(City).order_by(City.id).all()
+
+    for elem in city_list:
+        # inner loop retrieves state name
+        for obj in state_list:
+            if obj.id == elem.state_id:
+                state_name = obj.name
+        print("{}: ({}) {}".format(state_name, elem.id, elem.name))
